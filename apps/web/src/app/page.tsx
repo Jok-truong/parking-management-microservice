4a9a46f55ea6bcbe783252b4a02c439a54bdec7f
Huy Truong <o1key222k@gmail.com>
2024-10-01 20:07:31 +0700

    add validation script

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,5 +1,5 @@
-import { add } from "@parking-management-microservice/sample-lib";
+import { add } from '@parking-management-microservice/sample-lib'
 
 export default function Home() {
-  return <main className="text-red-500">Hello {add(1, 2)}</main>;
+  return <main className="text-red-500">Hello {add(1, 2)}</main>
 }
